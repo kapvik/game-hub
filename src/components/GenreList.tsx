7259491a223252aa1ab93b5be2cc3143696bde19
Victiriia Kapitulina <vika.kapitulina@gmail.com>
2024-06-25 16:32:48 +0300

    Add genres images

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/UseGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((gener) => (
-        <li key={gener.id}>{gener.name}</li>
+        <ListItem key={gener.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(gener.image_background)}
+            />
+            <Text fontSize="lg">{gener.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
